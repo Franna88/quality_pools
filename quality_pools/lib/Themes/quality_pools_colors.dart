a9d005d24df5cc673bbfc6d6b9c2e7baf865a0aa
Keita-lee <keita@barefootbytes.com>
2025-01-16 14:47:10 +0200

    adding all the readings and homepage features

---
+++ quality_pools/lib/Themes/quality_pools_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  final Color lightBlue = Color(0XFF1A8CF0);
+  final Color mediumBlue = Color(0XFF095BB2);
+  final Color darkBlue = const Color(0xFF002A6A);
+}
