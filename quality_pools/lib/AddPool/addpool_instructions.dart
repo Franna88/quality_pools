b8e9ca170a09f56bf89805c0ce639094af7fbe58
Lithakazi Sikwana <lithakazisikwana@yahoo.com>
2025-01-23 00:11:53 +0200

    Add pool new files

---
+++ quality_pools/lib/AddPool/addpool_instructions.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:quality_pools/CommonComponants/common_button.dart';
+import 'package:quality_pools/AddPool/pooIInfo.dart';
+import 'package:quality_pools/CommonComponants/quality_pool_header.dart';
+
+
+
+class AddPoolImage extends StatefulWidget {
+  const AddPoolImage({super.key});
+
+  @override
+  State<AddPoolImage> createState() => _AddPoolImageState();
+}
+
+class _AddPoolImageState extends State<AddPoolImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0XFF1A8CF0),
+              Color(0XFF095BB2),
+              Color(0xFF002A6A),
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            QualityPoolHeader(),
+                SizedBox(height: 20), // Add spacing between text and buttons
+                            CommonButton(
+              buttonText: 'Select Image',
+              onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const AddPoolInfo(),
+                    ),);
+              },
+            ),
+             SizedBox(height: 20), // Add spacing between text and buttons
+                            CommonButton(
+              buttonText: 'Continue',
+              onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const AddPoolInfo(),
+                    ),);
+              },
+            ),
+          
+            
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
